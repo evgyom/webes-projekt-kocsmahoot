8c4bc6565fb5bad07eed2fc096812190d6245041
Misi <tatai.misi@gmail.com>
2021-11-10 16:00:48 +0100

    main screens added, buttons working

--- kocsma-kahoot-frontend/src/App.vue
+++ kocsma-kahoot-frontend/src/App.vue
@@ -1,28 +1,126 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="top">
+      <h1>KOCSMAHOOT</h1>
+    </div>
+    <ul class="navbar">
+      <li v-for="item in NavBarButtons" :key="item.id">
+        <nav-barbutton
+          :label="item.label"
+          :type="item.type"
+          @updated-selected-screen="updateActionArea(item.type)"
+        >
+        </nav-barbutton>
+      </li>
+    </ul>
+    <div class="action-area-holder">
+      <join-game v-if="MainScreenType == 1"> </join-game>
+      <create-game v-if="MainScreenType == 2"> </create-game>
+      <how-to-play v-if="MainScreenType == 3"> </how-to-play>
+      <upload-game v-if="MainScreenType == 4"> </upload-game>
+    </div>
+
+    <div class="footer">
+      <p>Valami footer szöveg.</p>
+      <p>Valami footer szöveg.</p>
+      <p>{{ MainScreenType }}</p>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import JoinGame from "./components/mainScreenComponents/JoinGame.vue";
+import CreateGame from "./components/mainScreenComponents/CreateGame.vue";
+import HowToPlay from "./components/mainScreenComponents/HowToPlay.vue";
+import UploadGame from "./components/mainScreenComponents/UploadGame.vue";
+import NavBarbutton from "./components/smallItems/NavBarButton.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    NavBarbutton,
+    JoinGame,
+    CreateGame,
+    HowToPlay,
+    UploadGame,
+  },
+  data() {
+    return {
+      MainScreenType: 1,
+      NavBarButtons: [
+        { label: "JOIN GROUP", type: 1 },
+        { label: "CREATE GROUP", type: 2 },
+        { label: "HOW TO PLAY?", type: 3 },
+        { label: "UPLOAD QUIZ", type: 4 },
+      ],
+    };
+  },
+  methods: {
+    updateActionArea(newType) {
+      this.MainScreenType = newType;
+    },
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+html {
+  font-size: 1vw;
+  font-family: "Roboto", sans-serif;
+  color: white;
+  background-color: white;
+}
+</style>
+
+<style scoped>
+.top {
+  display: block;
+  background-color: lightslategray;
+  margin: 0;
+  padding: 5px;
+}
+
+.top h1 {
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  font-size: 4rem;
+  margin: 0;
+  padding: 0;
+}
+
+/* NAVBAR */
+
+.navbar {
+  height: auto;
+  flex: 100%;
+  background-color: lightgrey;
+  display: flex;
+  justify-content: space-evenly;
+  margin-top: 5px;
+}
+
+.navbar li {
+  display: block;
+  flex: 1;
+}
+
+/* ACTION AREA */
+
+.action-area-holder {
+  display: block;
+  min-height: 500px;
+  background-color: grey;
+  margin: 0;
+  padding: 0;
+}
+
+/* FOOTER */
+
+.footer {
+  height: auto;
+  flex: 100%;
+  background-color: lightgrey;
+  display: flex;
+  margin-top: 5px;
+  font-size: 1.2rem;
 }
 </style>
---
+++ kocsma-kahoot-frontend/src/components/smallItems/QuizListItem.vue
@@ -0,0 +1,33 @@
+
+<template>
+  <div class="quiz-list-item">
+    <h1>{{ title }}</h1>
+    <p>{{description}} </p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: { required: true, type: String },
+    description: {required: true, type: String}
+  },
+  methods: {
+        updateSelectedScreen() {
+          this.$emit('updated-selected-screen', this.type);
+        },
+  },
+};
+</script>
+
+<style scoped>
+
+h1 {
+  font-size: 2rem;
+  color: white;
+  display: block;
+  position: relative;
+  text-align: left;
+}
+
+</style>
